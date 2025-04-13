6c3015505dc650bc1eef15e6e14d5cd4f46ec34b
hen9wu <37192071+hen9wu@users.noreply.github.com>
2025-04-13 14:40:39 +0000

    Deploying to gh-pages from @ hen9wu/hen9wu.github.io@a1255c82a26c7bcfad41c9ade06da2eff756a14a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,21 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
@@ -426,10 +412,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-quote-from-bob-dennard-if-you-want-to-be-successful-attitude-is-everything-we-have-all-the-knowledge-you-need-and-you-ought-to-be-ready-to-have-the-attitude-to-do-something-with-it-robert-h-dennard",
-          title: 'Quote from Bob Dennard If you want to be successful, attitude is everything....',
-          description: "",
-          section: "News",},{id: "news-prof-wu-joined-peking-university-as-an-associate-professor",
+            },},{id: "news-prof-wu-joined-peking-university-as-an-associate-professor",
           title: 'Prof. Wu Joined Peking University as an Associate Professor',
           description: "",
           section: "News",handler: () => {
@@ -449,51 +432,36 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_4/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
+            },},{id: "projects-design-technology-co-optimization-dtco",
+          title: 'Design Technology Co-optimization (DTCO)',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-3d-integration",
+          title: '3D Integration',
+          description: "3D integration enables the continuous scaling beyond Moore&#39;s Law",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-the-stacking-technology",
+          title: 'The Stacking Technology',
+          description: "Flip FET is a revolutionary transistor stacking technology",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-dual-sided-circuits-design",
+          title: 'Dual-sided Circuits Design',
+          description: "The circuit design innovations for both sides of wafer",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-technology-definition",
+          title: 'Technology Definition',
+          description: "Pathfinding for Next-Gen Technology",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-teambuilding",
+          title: 'Teambuilding',
+          description: "The team",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
